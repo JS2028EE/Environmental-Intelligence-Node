7bc9c49734c4e39479f9894420e12876f37c9718
JS2028EE <jhostins099@icloud.com>
2026-09-07 00:27:43 -0400

    fix: remove BME280 and GPS sensor state

--- firmware/Sensors.h
+++ firmware/Sensors.h
@@ -5,11 +5,9 @@
 struct SensorData {
   float temperatureC=NAN, humidity=NAN; int lightRaw=0,soundRaw=0,heartRaw=0,hallRaw=0,waterRaw=0;
   bool tcrtDetected=false,irDetected=false,flameDetected=false; int heartRate=0; const char* heartSignal="WAIT";
-  bool mpuPresent=false,bmePresent=false,gpsFix=false;
+  bool mpuPresent=false;
   float accelX=NAN,accelY=NAN,accelZ=NAN,gyroX=NAN,gyroY=NAN,gyroZ=NAN,accelMagnitude=NAN,tiltDegrees=NAN;
   bool motionDetected=false,impactDetected=false,tiltDetected=false;
-  float pressureHpa=NAN,relativeAltitudeM=NAN,surfaceDeltaM=NAN; const char* surfaceState="UNKNOWN";
-  double latitude=NAN,longitude=NAN; float gpsAltitudeM=NAN,gpsSpeedKmh=NAN,estimatedSpeedKmh=NAN,courseDegrees=NAN; uint32_t satellites=0;
 };
 extern SensorData sensors;
 extern SystemStatus systemStatus;
