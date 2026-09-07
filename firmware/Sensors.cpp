4baa26acade8964ccdf78226b6a07af87c6cd240
JS2028EE <jhostins099@icloud.com>
2026-09-07 00:27:39 -0400

    fix: remove unused BME280 and GPS firmware dependencies

--- firmware/Sensors.cpp
+++ firmware/Sensors.cpp
@@ -4,12 +4,10 @@
 #include <DHT.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-#include <TinyGPSPlus.h>
 
-static DHT dht(PIN_DHT,DHTTYPE); static Adafruit_MPU6050 mpu; static Adafruit_BME280 bme; static TinyGPSPlus gps; static HardwareSerial gpsSerial(1);
+static DHT dht(PIN_DHT,DHTTYPE); static Adafruit_MPU6050 mpu;
 SensorData sensors; SystemStatus systemStatus=STATUS_NORMAL;
-static unsigned long flameHighSince=0,lastBeatTime=0; static int heartBaseline=2048; static bool heartAboveThreshold=false; static float referencePressureHpa=NAN;
+static unsigned long flameHighSince=0,lastBeatTime=0; static int heartBaseline=2048; static bool heartAboveThreshold=false;
 
 void sensorsBegin(){
   pinMode(PIN_TCRT,INPUT); pinMode(PIN_IR,INPUT); pinMode(PIN_FLAME,INPUT_PULLDOWN); dht.begin();
@@ -17,9 +15,6 @@ void sensorsBegin(){
   sensors.mpuPresent=mpu.begin(MPU6050_ADDRESS);
   if(!sensors.mpuPresent) sensors.mpuPresent=mpu.begin(MPU6050_ALT_ADDRESS);
   if(sensors.mpuPresent){ mpu.setAccelerometerRange(MPU6050_RANGE_8_G); mpu.setGyroRange(MPU6050_RANGE_500_DEG); mpu.setFilterBandwidth(MPU6050_BAND_21_HZ); }
-  sensors.bmePresent=bme.begin(BME280_ADDRESS); if(!sensors.bmePresent) sensors.bmePresent=bme.begin(BME280_ALT_ADDRESS);
-  if(sensors.bmePresent) referencePressureHpa=bme.readPressure()/100.0f;
-  gpsSerial.begin(GPS_BAUD,SERIAL_8N1,PIN_GPS_RX,-1);
 }
 
 void sensorsReadFast(){
@@ -29,18 +24,11 @@ void sensorsReadFast(){
   bool flameActive=FLAME_ACTIVE_LOW?(flameRaw==LOW):(flameRaw==HIGH);
   if(flameActive){if(flameHighSince==0)flameHighSince=millis(); sensors.flameDetected=(millis()-flameHighSince>=FLAME_CONFIRM_MS);} else {flameHighSince=0;sensors.flameDetected=false;}
 
-  while(gpsSerial.available()) gps.encode(gpsSerial.read());
-  sensors.gpsFix=gps.location.isValid() && gps.location.age()<3000; if(sensors.gpsFix){sensors.latitude=gps.location.lat();sensors.longitude=gps.location.lng();}
-  if(gps.altitude.isValid()) sensors.gpsAltitudeM=gps.altitude.meters();
-  if(gps.speed.isValid()){sensors.gpsSpeedKmh=gps.speed.kmph();sensors.estimatedSpeedKmh=sensors.gpsSpeedKmh;}
-  if(gps.course.isValid()) sensors.courseDegrees=gps.course.deg(); if(gps.satellites.isValid()) sensors.satellites=gps.satellites.value();
-
   if(sensors.mpuPresent){ sensors_event_t a,g,t; mpu.getEvent(&a,&g,&t); sensors.accelX=a.acceleration.x;sensors.accelY=a.acceleration.y;sensors.accelZ=a.acceleration.z;sensors.gyroX=g.gyro.x;sensors.gyroY=g.gyro.y;sensors.gyroZ=g.gyro.z;
     sensors.accelMagnitude=sqrtf(sensors.accelX*sensors.accelX+sensors.accelY*sensors.accelY+sensors.accelZ*sensors.accelZ);
     sensors.tiltDegrees=atan2f(sqrtf(sensors.accelX*sensors.accelX+sensors.accelY*sensors.accelY),fabsf(sensors.accelZ))*57.2958f;
     sensors.motionDetected=fabsf(sensors.accelMagnitude-9.80665f)>MOTION_ACCEL_THRESHOLD_MS2; sensors.impactDetected=sensors.accelMagnitude>IMPACT_ACCEL_THRESHOLD_MS2; sensors.tiltDetected=sensors.tiltDegrees>TILT_THRESHOLD_DEG;
   }
-  if(sensors.bmePresent){ sensors.pressureHpa=bme.readPressure()/100.0f; sensors.relativeAltitudeM=44330.0f*(1.0f-powf(sensors.pressureHpa/referencePressureHpa,0.19029495f)); sensors.surfaceDeltaM=sensors.relativeAltitudeM; if(sensors.surfaceDeltaM>SURFACE_DEADBAND_M)sensors.surfaceState="ABOVE"; else if(sensors.surfaceDeltaM<-SURFACE_DEADBAND_M)sensors.surfaceState="BELOW"; else sensors.surfaceState="LEVEL"; }
 }
 
 void sensorsReadSlow(){sensors.temperatureC=dht.readTemperature();sensors.humidity=dht.readHumidity();}
